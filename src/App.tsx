fd066eaf103e4d91b59b55586236f402297c14a2
naveeenmuthh <naveenmuthu05@gmail.com>
2025-05-13 14:50:54 +0530

    responsive-layout-completed

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,40 @@ import PostComponent from './components/PostComponent'
 import RightBarComponent from './components/RightBarComponent'
 import StoryComponent from './components/StoryComponent'
 
+import {useMediaQuery} from "react-responsive";
+import LeftBarResponsive from './components/leftBarResponsive'
+import LeftBarMobile from './components/leftBarMobile'
+
 function App() {
+
+  const screen1 = useMediaQuery({query:'(max-width:1265px)'});
+  const screen2 = useMediaQuery({query:'(max-width:1160px)'});
+  const screen3 = useMediaQuery({query:'(max-width:768px)'});
+
+  console.log(screen1,screen2,screen3,"hi");
+
 
   return (
     <>
-       <div className="main-container">
-       <LeftBarComponent/>
+     {!screen3? <div className="main-container">
+       {screen1?<LeftBarResponsive/>:<LeftBarComponent/>}
            <div className="movable-container">
            <div className="mid-container">
        <StoryComponent/>
        <PostComponent/>
        </div>
-       <RightBarComponent/> 
+       {!screen2 && <RightBarComponent/> }
            </div>
+       </div> : <div className='main-mobile-container'>
+       <div className="movable-container">
+           <div className="mid-container">
+       <StoryComponent/>
+       <PostComponent/>
        </div>
+       {!screen2 && <RightBarComponent/> }
+           </div>
+           <LeftBarMobile/>
+        </div>}
     </>
   )
 }
