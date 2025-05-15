0b25717498bc94f81271848aeadf357933e2777d
naveeenmuthh <naveenmuthu05@gmail.com>
2025-05-15 17:42:46 +0530

    new repo

--- src/components/StoryComponent.tsx
+++ src/components/StoryComponent.tsx
@@ -1,7 +1,17 @@
+import { useRef } from "react";
 import InstaProfileImg from "../../public/insta_profile_sample.jpg";
+
 
 const StoryComponent = () => {
 
+  const sliderRef = useRef<HTMLDivElement | null>(null);
+
+  const scroll = (direction:number)=>{
+const scrollAmount = 60; 
+if(sliderRef.current){
+  sliderRef.current.scrollBy({left:direction*scrollAmount, behavior:"auto"});
+}
+  }
 
 const data = [{
  profile_name:"user_1",
@@ -35,12 +45,21 @@ const data = [{
   profile_name:"user_1",
   prof_img:<img src={InstaProfileImg} className="story-profile-img"></img>,
   redirect:"story_user_1", 
+ },{
+  profile_name:"user_1",
+  prof_img:<img src={InstaProfileImg} className="story-profile-img"></img>,
+  redirect:"story_user_1", 
+ },{
+  profile_name:"user_1",
+  prof_img:<img src={InstaProfileImg} className="story-profile-img"></img>,
+  redirect:"story_user_1", 
  }
 ]
 
   return (
     <div className="story-container">
-        <div className="story-items-container">
+      <button className="prev-container nav-button" onClick={()=>scroll(-1)}>prev</button>
+        <div className="story-items-container" ref={sliderRef}>
         {data.map((d,i)=>{
 
 return <div className="story-items" key={i}>
@@ -49,6 +68,7 @@ return <div className="story-items" key={i}>
 </div>
 })}
         </div>
+        <button className="next-container nav-button" onClick={()=>scroll(1)}>next</button>
     </div>
   )
 }
