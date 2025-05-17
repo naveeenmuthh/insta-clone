ef12125ea8d82192306c7f2134fc498908cc4440
naveeenmuthh <naveenmuthu05@gmail.com>
2025-05-17 17:06:13 +0530

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/instagram-clone/",
+  base:"/insta-clone/",
   plugins: [react()],
 })
